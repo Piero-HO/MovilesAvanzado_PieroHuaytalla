db3edba1ed7d8c5678f26f1fcc582253cec14998
Pierito <piero.huaytalla@tecsup.edu.pe>
2026-09-15 11:36:25 -0500

    feat: calcular IGV y descuentos de cursos

--- Semana04/actividad01.playground/Contents.swift
+++ Semana04/actividad01.playground/Contents.swift
@@ -17,3 +17,44 @@ struct Curso {
         precio * Double(cantidad)
     }
 }
+
+struct FacturaCursos {
+    let alumno: Alumno
+    let cursos: [Curso]
+
+    private let porcentajeIGV = 0.18
+    private let porcentajeDescuentoCantidad = 0.10
+    private let descuentoAlumnoTecsup = 400.00
+
+    var cantidadCursos: Int {
+        cursos.reduce(0) { $0 + $1.cantidad }
+    }
+
+    var subtotal: Double {
+        cursos.reduce(0) { $0 + $1.importe }
+    }
+
+    var igv: Double {
+        subtotal * porcentajeIGV
+    }
+
+    var totalConIGV: Double {
+        subtotal + igv
+    }
+
+    var cumpleCantidadMinima: Bool {
+        cantidadCursos >= 3
+    }
+
+    var descuentoPorCantidad: Double {
+        cumpleCantidadMinima ? totalConIGV * porcentajeDescuentoCantidad : 0
+    }
+
+    var descuentoEspecialTecsup: Double {
+        cumpleCantidadMinima && alumno.esAlumnoTecsup ? descuentoAlumnoTecsup : 0
+    }
+
+    var totalFinal: Double {
+        max(0, totalConIGV - descuentoPorCantidad - descuentoEspecialTecsup)
+    }
+}

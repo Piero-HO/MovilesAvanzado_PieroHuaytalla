82ae66c8b34967dff22057ea840052fd09552e35
Pierito <piero.huaytalla@tecsup.edu.pe>
2026-09-15 11:40:51 -0500

    feat: agregar clase base cliente

--- Semana04/actividad02.playground/Contents.swift
+++ Semana04/actividad02.playground/Contents.swift
@@ -1,3 +1,33 @@
 import Foundation
 
 // Actividad propuesta 02: herencia y polimorfismo con clientes bancarios.
+
+class Cliente {
+    let codigo: String
+    let direccion: String
+    let fechaDeRegistro: String
+    let numeroCuenta: String
+    let montoMinimoApertura: Double
+
+    init(
+        codigo: String,
+        direccion: String,
+        fechaDeRegistro: String,
+        numeroCuenta: String,
+        montoMinimoApertura: Double
+    ) {
+        self.codigo = codigo
+        self.direccion = direccion
+        self.fechaDeRegistro = fechaDeRegistro
+        self.numeroCuenta = numeroCuenta
+        self.montoMinimoApertura = montoMinimoApertura
+    }
+
+    func mostrarDatos() {
+        print("📄 Código: \(codigo)")
+        print("📍 Dirección: \(direccion)")
+        print("🗓️ Fecha de registro: \(fechaDeRegistro)")
+        print("🏦 N.º Cuenta: \(numeroCuenta)")
+        print(String(format: "💰 Monto mínimo de apertura: S/ %.2f", montoMinimoApertura))
+    }
+}

f1dc2e35bbcaf7745aa258b5e211e60c5fe20d24
Pierito <piero.huaytalla@tecsup.edu.pe>
2026-09-15 11:36:09 -0500

    feat: agregar modelos de alumno y curso

--- Semana04/actividad01.playground/Contents.swift
+++ Semana04/actividad01.playground/Contents.swift
@@ -1,3 +1,19 @@
 import Foundation
 
 // Actividad propuesta 01: facturación de cursos libres de Tecsup.
+
+struct Alumno {
+    let nombre: String
+    let dni: String
+    let esAlumnoTecsup: Bool
+}
+
+struct Curso {
+    let nombre: String
+    let precio: Double
+    let cantidad: Int
+
+    var importe: Double {
+        precio * Double(cantidad)
+    }
+}

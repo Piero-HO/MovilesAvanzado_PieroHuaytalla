668cd8ea72de00411896427ddf50495f56bfdba9
Pierito <piero.huaytalla@tecsup.edu.pe>
2026-09-21 17:39:01 -0500

    feat: agregar calculadora de IMC

---
+++ Semana05/Laboratorio05_1/Laboratorio05_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Laboratorio05_1
+//
+//  Created by Piero Huaytalla on 16/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtSuelto: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Semana05/Laboratorio05_2/Laboratorio05_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Laboratorio05_2
+//
+//  Created by Piero Huaytalla on 16/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
